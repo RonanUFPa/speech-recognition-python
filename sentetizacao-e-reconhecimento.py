e3bed6729412ba35020e2d62c9469f64653e546c
Ronan Silva <contato.ronansilva@gmail.com>
2019-03-25 13:03:34 -0300

    Atualizando script
    Atualizando algumas dependências de bibliotecas.

--- sentetizacao-e-reconhecimento.py
+++ sentetizacao-e-reconhecimento.py
@@ -10,12 +10,12 @@ print(rec.recognize_google(frase, language='pt')) #transformando nossa fala em t
 
 
 #TRANSFORMANDO TEXTO EM FALA
-import pyttsx # importamos o modúlo
+import pyttsx # importamos o modúlo de sintetização
 en = pyttsx.init() # meotodo init seleciona um ending de sintetização, no caso o espeak
 en.say("Hello, I am Ronan") # o metodo say para dizer o que queremos
 en.say(Nice to meet you) 
 en.runAndWait() # para ouvir o que foi escrito
-en.setProperty('voice', b'brazil') # mudamos a propriedade setando pelo id para pt-br, o lemento b diz que a string está em bytes
+en.setProperty('voice', b'brazil') # mudamos a propriedade setando pelo id para pt-br, o segundo argumento:'b' diz que a string está em bytes
 en.say('Olá, tudo bem?')
 en.runAndWait()
 
@@ -25,4 +25,4 @@ voz = gTTS("Olá, tudo bem?", lang ="pt") # guardamos o nosso texto na variavel 
 voz.save("voz.mp3") #salvamos com o comando save em mp3
 
 import subprocess as s #importamos o subprocess e renomeamos a s
-s.call(['MPC-HC', 'voz.mp3']) #com o comando call roda nosso comando de voz no player escolhido
+s.call(['MPC-HC', 'voz.mp3']) #com o comando call roda nosso comando de voz no player escolhido, no meu caso foi o MPC-HC
